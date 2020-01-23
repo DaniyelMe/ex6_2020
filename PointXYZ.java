c423d812ea912b65313fc4d418c337ee6fb5682a
Daniyel <DaniyelMenda@Gmail.com>
2020-01-23 14:58:43 +0200

    feat: Adds  PointXYZ functionality

--- PointXYZ.java
+++ PointXYZ.java
@@ -6,28 +6,60 @@ public class PointXYZ extends PointXY {
     this.z = z;
   }
 
+  public PointXYZ(double x, double y, double z, boolean isCartesian) {
+    super(x, y);
+    this.z = z;
+
+    if (!isCartesian) {
+      // polar coordinate system
+      double r = getR();
+      double theta = getTheta();
+      double phi = getPhi();
+
+      setX(r * Math.sin(theta) * Math.cos(phi));
+      setY(r * Math.sin(theta) * Math.sin(phi));
+      this.z = r * Math.cos(theta);
+    }
+
+  }
+
   public double getZ() {
     return z;
   }
 
-  
+  public void setZ(double z) {
+    this.z = z;
+  }
 
-//   +getR():double
-// +getTheta():double
-// +getPhi():double
-// +toString():String
+  @Override
+  public double getR() {
+    // Return the radial distance r.
+
+    double x = getX();
+    double y = getX();
+
+    return Math.sqrt(x * x + y * y + z * z);
+  }
+
+  @Override
+  public double getTheta() {
+    // Return the θ.
+    return Math.acos(z / getR());
+  }
+
+  public double getPhi() {
+    // Return the ϕ.
+    return Math.atan(getY() / getX());
+  }
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj) {
+    // If it's the same point
+    if (this == obj)
       return true;
-    }
-    if (obj == null || getClass() != obj.getClass()) {
+
+    if (obj == null || getClass() != obj.getClass() || !super.equals(obj))
       return false;
-    }
-    if (!super.equals(obj)) {
-      return false;
-    }
 
     PointXYZ pointXYZ = (PointXYZ) obj;
 
@@ -37,6 +69,8 @@ public class PointXYZ extends PointXY {
 
   @Override
   public String toString() {
-    return "[" + getX() + "," + getY() + "," + getZ() + "]";
+    return "(x,y,z)=(" + getX() + ", " + getY() + ", " + getZ() + ")" + "(r,theta, phi)=(" + getR() + ", " + getTheta()
+        + ", " + getPhi() + ")";
+
   }
 }
